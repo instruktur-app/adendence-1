639e60268082ae0d70dcd5a62c693b07bf74508a
Asrul Harahap <admin@asrul.dev>
2022-10-13 19:42:24 +0700

    :fire: penjelasan reduce

--- lima.js
+++ lima.js
@@ -1,14 +1,22 @@
 function biggerElement(arr) {
-    const result = arr.reduce(function(previous,current){
-        if(current>previous){
+    const result = arr.reduce(function(previous, current) {
+        if(current > previous){
             return current
-        }else{
+        } else{
             return previous
         }
-    })
+    }, 0)
     return result
     
   }
   
   console.log(biggerElement([1,3,2,4]))
   // output: 4
+
+// p = 0   c = 1   c > p ====> c => 1
+// p = 1   c = 3   c > p ====> c => 3
+// p = 3   c = 2   c > p ====> p => 3
+// p = 3   c = 4   c > p ====> c => 4
+// p = 4   c ????
+
+// return 4
